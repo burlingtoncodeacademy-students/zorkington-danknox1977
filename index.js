b50eb684fc195e8eeebb603c7655435b983298af
danknox1977 <127804873+danknox1977@users.noreply.github.com>
2023-04-20 12:49:54 -0400

    First working state machine for movement added

--- index.js
+++ index.js
@@ -13,44 +13,133 @@ async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+On the door is a handwritten sign. `;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+
+
+
+  console.log('Now write your code to make this work! ');
   process.exit();
 }
 
 
 //questions about state machines
+// -------------------------------------MovementStateMachine------------------------------------------------ //
+
+const bldgMap = {
+  'Front Porch': {'Driveway': 000, 'Foyer': 250},
+    'Driveway': {'Front Porch': 250, 'Car' : 000},
+    'Foyer': {'Front Porch': 250, 'Hall': 250, 'Stairs btw 1st and 2nd': 5000, 'Dining': 250},
+    'Dining': {'Foyer': 250, 'Kitchen': 250}
+
+}
 
 
-* The current room
-  * Room description (immutable)
-  * Room connection (immutable)
-  * Room inventory (mutable)
-* Other rooms
-  * Room description (immutable)
-  * Room connection (immutable)
-  * Room inventory (mutable)
-* The player
-  * Player inventory (mutable)
-  * Player status (mutable)
+class Player {
+  constructor (name, mainSkill, dispo, chi, currentLocation) {
+  this.name = name
+  this.mainSkill = mainSkill
+  this.dispo = dispo
+  this.chi = chi
+  this.currentLocation = currentLocation
+ 
+  }
+//method to move between locations
+  move (newLocation) {
+      const possMoves = bldgMap[this.currentLocation]
+      if (newLocation in possMoves) {
+
+          if (possMoves[newLocation] > this.chi) {
+          console.log('Alas, Grasshopper, you are not ready to move here. ')
+          //chi refers to the value of the key of whatever newLocation they're referring
+          } else {
+
+          console.log(`Moving to ${newLocation}... `)
+          this.currentLocation = newLocation
+          }
+      } else {
+
+        console.log(`Cannot move to ${newLocation}. Try Again. `) 
+      }
+  }
+//method to list possible moves from current location
+//doesn't work yet
+  getMoves () {
+      const possMoves = bldgMap[this.currentLocation]
+      for (const m in possMoves) {
+          if (possMoves[m] <= this.chi) {
+              console.log(m.toUpperCase())
+          }
+      }
+  }        
+}      
+
+  
+
+
+//add method to move  somewhere else
+
+const playerOne = new Player('Chuck', 'Willful Ignorance', 'Smug', 40000, 'Front Porch')
+
+console.log(playerOne)
+
+console.log(playerOne.currentLocation.getMoves)
+
+playerOne.move('Driveway')
+
+console.log(playerOne.currentLocation)
+console.log(playerOne.chi)
+
+console.log(playerOne.getMoves)
+
+playerOne.move('Front Porch')
+
+console.log(playerOne.currentLocation)
+console.log(playerOne.chi)
+
+console.log(playerOne.getMoves)
+
+playerOne.move('Foyer')
+
+console.log(playerOne.currentLocation)
+console.log(playerOne.chi)
+
+console.log(playerOne.getMoves)
+
+
+// * The current room
+//   * Room description (immutable)
+//   * Room connection (immutable)
+//   * Room inventory (mutable)
+// * Other rooms
+//   * Room description (immutable)
+//   * Room connection (immutable)
+//   * Room inventory (mutable)
+// * The player
+//   * Player inventory (mutable)
+//   * Player status (mutable)
+
+// ---------------------------------Room Classes--------------------------------------------------- //
 
 //Room Class
 class Room {
-  constructor (name, connections, itemsOfInterest) {
+  constructor (name, connections, itemsOfInterest, elevation) {
     this.name = name
-    this.connections = connections
+    this.connections = connections //mutable?
     this.itemsOfInterest = location
+    this.elevation = elevation
+
   }
 }
 
-class Floor {
-  constructor (name, elevation, statusEffect) {
-    this.name = name
-    this.elevation = elevation
-    this.statusEffect = statusEffect
-  }
-}
+
+// class Floor extends Room {
+//   constructor (name, elevation, statusEffect) {
+//     this.name = name
+//     this.elevation = elevation
+//     this.statusEffect = statusEffect
+//   }
+// }
 
 // class Reality extends Location {
 //   constructor (name, mutable?, immutable?) {
@@ -59,45 +148,9 @@ class Floor {
 
 
 
-// -------------------------------------ClassDeclaration-------------------------------------------------- //
-class Room {
+// -------------------------------------Item ClassDeclaration-------------------------------------------------- //
 
-  constructor (name, currOpen, location) {
-    this.name = name
-    
-    this.currOpen = currOpen  // boolean
-    this.location = location  // instance of Location class
-    this.inventory = new Inventory(this)
-  }
 
-  addInv (Inv) {
-    total = 0
-    for (const quantitem of Inv) {
-      const [item, quantity] = quantitem
-      if (this.inventory.items[item.id] < quantity) {
-        // console.log('Cannot process cart, not enough of some item(s)')
-        // return
-      }
-      // total += (item.cost + (item.cost * this.location.taxpct).toPrecision(2)) * quantity
-
-    }
-  }
-}
-
-class Inventory {
-  constructor (store) {
-    this.store = store
-    this.items = {}   // object storing {item id: quantity}
-  }
-
-  AddItem (item, quantity) {
-    if (item.id in this.items) {
-      this.items[item.id] += quantity
-    } else {
-      this.items[item.id] = quantity
-    }
-  }
-}
 
 class RoomItems {
   constructor (id, name, value, weight) {
@@ -110,6 +163,7 @@ class RoomItems {
 
 
 // -------------------------------------ClassPlayerInventory-------------------------------------------------- //
+
 class Inventory {
   constructor () {
     this.items = []  // array of two-element arrays, [[item, quant], [item, quant], etc.]
@@ -119,3 +173,11 @@ class Inventory {
     this.items.push([item, quantity])
   }
 }
+
+
+// -------------------------------------playerInput------------------------------------------------- //
+
+// should take a 2 word command and then differentiate between action and item
+
+// async function await ask 
+

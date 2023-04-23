e345f6aca5d549c03e655070ec891a876bfe7fe8
danknox1977 <127804873+danknox1977@users.noreply.github.com>
2023-04-23 16:33:09 -0400

    answer logic can change two word input to a string of all lowercase

--- index.js
+++ index.js
@@ -20,29 +20,56 @@ let grayText = "\033[90m";
 
 // ---------------------------------------ClassPlayerAndRelated----------------------------------------------- //
 
+class Item {
+  constructor(name, what) {
+    this.name = name;
+    this.what = what;
+  }
+}
+
 class Player {
-  constructor(name, mainSkill, dispo, chi, currentLocation, Inventory) {
+  constructor(name, mainSkill, dispo, chi, Room, Inventory) {
     this.name = name;
     this.mainSkill = mainSkill;
     this.dispo = dispo;
     this.chi = chi;
-    this.currentLocation = currentLocation;
+    this.Room = Room;
     this.Inventory = Inventory;
   }
-}
 
-// -------------------------------------ClassPlayerInventory-------------------------------------------------- //
+  //method to move between locations
 
-class Inventory extends Player {
-  constructor() {
-    this.item = []; // array of two-element arrays, [[item, quant], [item, quant], etc.]
+  move(newLocation) {
+    const possMoves = bldgMap[this.Room];
+    if (newLocation in possMoves) {
+      if (possMoves[newLocation] > this.chi) {
+        console.log("Alas, Grasshopper, you are not ready to move here. ");
+        //chi refers to the value of the key of whatever newLocation they're referring
+      } else {
+        console.log(`Moving to ${newLocation}... `);
+        this.Room = newLocation;
+      }
+    } else {
+      console.log(`Cannot move to ${newLocation}. Try Again. `);
+    }
   }
 
-  // -------------------------------------PlayerAddInventoryFunction-------------------------------------------------- //
-  addItem(item, quantity) {
-    this.items.push([item, quantity]);
+  //method to list possible moves from current location
+  //doesn't work yet
+  getMoves() {
+    const possMoves = bldgMap[this.Room];
+    for (const m in possMoves) {
+      if (possMoves[m] <= this.chi) {
+        console.log(m.toLowerCase());
+      } else {
+        console.log("there are no moves, you are trapped\nGaMe OvEr!!!?!!?");
+        process.exit();
+      }
+    }
   }
 }
+
+// -----------------------------------PlayerOne------------------------------------------------------ //
 
 const playerOne = new Player(
   "Chuck",
@@ -50,12 +77,24 @@ const playerOne = new Player(
   "Smug",
   40000,
   "Front Porch",
-  ["cellphone, 1", "uber eats, 1"]
+  ["cellphone", "uber eats"]
 );
 
-// ---------------------------------GameFiles--------------------------------------------------- //
-var gameStatus = "pending"
+// -------------------------------------ClassPlayerInventory-------------------------------------------------- //
 
+class Inventory extends Player {
+  constructor(item) {
+    this.item = item;
+  }
+
+  // -------------------------------------PlayerAddInventoryMethod-------------------------------------------------- //
+  addItem(item) {
+    this.Inventory.push(item);
+  }
+}
+
+// ---------------------------------GameFiles--------------------------------------------------- //
+var gameStatus = "pending";
 
 const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
@@ -70,129 +109,168 @@ const prompt = `What do you do? >_`;
 start();
 
 async function start() {
-  
-  gameStatus = "start"
+  gameStatus = "start";
 
-  console.log(welcomeMessage)
+  console.log(welcomeMessage);
 
   while (gameStatus !== "end") {
     let answer = await ask(prompt);
+
+    // -----------------------------------AnswerLogic--------------------------------------------------- //
+
+    var parseAnswer = [];
+    parseAnswer.push(answer.toLowerCase().split(" "));
+    // var parseAnswerB = [];
+    // parseAnswerB = parseAnswer.map(element => {
+    //   return element.toLowerCase();
+    // });
+
+
+  
+
+    console.log(parseAnswer);
+  
+    console.log(word1)
+    console.log(word2)
     
-    if (answer == "checkStatus") {
-    console.log(playerOne.dispo);
+    
+    //if for inventory
+    if (commands.inventory.includes(word1)) {
+      inventory();
 
-    //else if for menu
+      //else if for getMoves
+    } else if (commands.possibleMoves.includes(word1)) {
+      console.log(playerOne.getMoves());
+
+      //else if for menu
     } else if (answer == "menu") {
-    console.log("menu");
+      console.log("menu");
 
-    // else if for where?
-    } else if (answer == "where") {
-    console.log(playerOne.currentLocation);
+      // else if for look
+    } else if (commands.look.includes(word1)) {
+      console.log(playerOne.Room.what);
 
-    //else for help
+      //else if for status
+    } else if (commands.checkStatus.includes(word1)) {
+      console.log(playerOne.dispo);
+
+      //else if for take
+    } else if (commands.take.includes(word1)) {
+      console.log("menu");
+
+      //else if for drop
+    } else if (commands.drop.includes(word1)) {
+      console.log("menu");
+
+      //else if for use
+    } else if (commands.use.includes(word1)) {
+      console.log("menu");
+
+      //else if for move
+    } else if (commands.move.includes(word1)) {
+      console.log("menu");
+
+      //else if for Help!
+    } else if (commands.helpList.includes(word1)) {
+      console.log("menu");
+
+      //else if for quit
+    } else if (commands.quit.includes(word1)) {
+      quit();
+
+      //else for help
     } else {
-    console.log(`type "help" for a list commands`);
-    }
-
-
-
-}
-//else for exit
-process.exit();
-}
-// -------------------------------------MovementStateMachine------------------------------------------------ //
-
-
-const bldgMap = {
-  "Front Porch": { 
-    Driveway: 000,
-    Foyer: 250
-  },
-  Driveway: {
-    "Front Porch": 250,
-    Car: 000
-  },
-  Foyer: {
-    "Front Porch": 250,
-    Hall: 250,
-    "Stairs btw 1st and 2nd": 5000,
-    Dining: 250,
-  },
-  Dining: {
-    Foyer: 250,
-    Kitchen: 250
-  },
-
-
-
-  //method to move between locations
-
-  move(newLocation) {
-    const possMoves = bldgMap[this.currentLocation];
-    if (newLocation in possMoves) {
-      if (possMoves[newLocation] > this.chi) {
-        console.log("Alas, Grasshopper, you are not ready to move here. ");
-        //chi refers to the value of the key of whatever newLocation they're referring
-      } else {
-        console.log(`Moving to ${newLocation}... `);
-        this.currentLocation = newLocation;
-      }
-    } else {
-      console.log(`Cannot move to ${newLocation}. Try Again. `);
-    }
-  },
-
-  //method to list possible moves from current location
-  //doesn't work yet
-  getMoves() {
-    const possMoves = bldgMap[this.currentLocation];
-    for (const m in possMoves) {
-      if (possMoves[m] <= this.chi) {
-        console.log(m.toLowerCase());
-      } else {
-        console.log("there are no moves, you are trapped\nGaMe OvEr!!!?!!?");
-        process.exit();
-      }
+      console.log(`type "help" for a list commands`);
     }
   }
+  //else for exit
+  process.exit();
 }
 
+// -------------------------------------MovementStateMachine------------------------------------------------ //
 
-// playerOne.move("Driveway");
-
-// console.log(playerOne.currentLocation);
-// console.log(playerOne.chi);
-
-// console.log(playerOne.getMoves);
-
-
-
-
-// * The current room
-//   * Room description (immutable)
-//   * Room connection (immutable)
-//   * Room inventory (mutable)
-// * Other rooms
-//   * Room description (immutable)
-//   * Room connection (immutable)
-//   * Room inventory (mutable)
-// * The player
-//   * Player inventory (mutable)
-//   * Player status (mutable)
+const bldgMap = {
+  frontPorch: {
+    driveway: 000,
+    foyer: 250,
+  },
+  driveway: {
+    frontPorch: 250,
+    car: 000,
+  },
+  foyer: {
+    frontPorch: 250,
+    hall: 250,
+    stairs1st2nd: 5000,
+    dining: 250,
+  },
+  dining: {
+    foyer: 250,
+    kitchen: 250,
+  },
+};
 
 // ---------------------------------Room Classes--------------------------------------------------- //
 
 //Room Class
 class Room {
-  constructor(name, connections, RoomItem, locked, elevation) {
+  constructor(name, connections, what, Item, locked) {
     this.name = name;
-    this.connections = connections; //mutable?
-    this.RoomItem = RoomItem; //extended to different class
-    this.locked = Boolean;
-    this.elevation = elevation;
+    this.what = what;
+    this.Item = [];
+    this.locked = locked;
   }
 }
 
+const driveway = new Room(
+  "Driveway",
+  "You look back at where you parked your car and hope you won't have to leave it idling too long...",
+  ["car", "salvation"],
+  true
+);
+const car = new Room(
+  "Your Car",
+  "A late model Kia Sorrento, idling obediently, lights on -with some muted music wafting from a slightly ajar window.",
+  ["Deliverence", "gasoline", "Best Of Queen cassette tape"],
+  true
+);
+const frontPorch = new Room(
+  "Front Porch",
+  "The area before the entrance could better be described as a sidewalk",
+  ["Front Door"],
+  false
+);
+
+const foyer = new Room(
+  "Foyer",
+  "Or, antechamber, looks deserted only the sound of a distant television betrays the\n prescence of other humans. There is a stair going up and a hall to your left.",
+  ["Directory"],
+  true
+);
+const hall = new Room(
+  "Hallway",
+  "A well lit hallway leading around the staircase going up to the second floor, there\n are doors closed doors leading off to the right and it makes a left hand turn\n into shadow...",
+  ["Front Door"],
+  false
+);
+const dining = new Room(
+  "Dining Room",
+  "A formal dining room, not the kind for everyday eating, covered in periodicals and office supplies.",
+  ["Outdoor Life Magazine", "Ink Pen"],
+  false
+);
+const kitchen = new Room(
+  "Kitchen",
+  "A lovely and well kept kitchen, nobody cooks here",
+  ["Fridge", "cabinet under sink", "microwave", "silverware drawer"],
+  false
+);
+const stairs1st2nd = new Room(
+  "Stairs between 1st & 2nd Floor",
+  "A grand wooden staircase, the steps are just slightly too tall, as if made for someone a little taller than you are.",
+  ["Railling", "Skateboard"],
+  true
+);
 // class Floor extends Room {
 //   constructor (name, elevation, statusEffect) {
 //     this.name = name
@@ -208,32 +286,70 @@ class Room {
 
 // -------------------------------------Item ClassDeclaration-------------------------------------------------- //
 
-class RoomItem extends Room {
-  constructor(id, name, value, weight) {
-    this.id = id;
-    this.name = name;
-    this.value = value;
-    this.weight = weight;
-  }
-}
-
-// -------------------------------------playerInput------------------------------------------------- //
-
-// should take a 2 word command and then differentiate between action and item
-
-// async function await ask
+// class Item {
+//   constructor(id, name, value, weight, pickup) {
+//     this.id = id;
+//     this.name = name;
+//     this.value = value;
+//     this.weight = weight;
+//     this.pickup = pickup;
+//   }
+// }
 
 // ------------------------------------PlayerCommands------------------------------------------------ //
 
 let commands = {
   inventory: ["i", "inventory", "inv"],
-  possibleMoves: ["where to"],
-  move: ["move"],
-  take: ["take"],
-  drop: ["drop"],
-  use: ["use"],
+  possibleMoves: ["where to", "where can I go", "possible moves"],
+  move: ["m", "move"],
+  take: ["t", "take"],
+  drop: ["d", "drop"],
+  use: ["u", "use"],
   look: ["l", "look"],
   helpList: ["h", "help"],
   checkStatus: ["status", "check status"],
-  menu: ["m", "menu"]
+  menu: ["m", "menu"],
+  quit: ["exit", "end", "quit"],
 };
+
+function inventory() {
+  console.log(playerOne.Inventory);
+}
+
+function move() {
+  playerOne.Room = newLocation;
+}
+
+function take() {
+  playerOne.Inventory.addItem(item);
+}
+
+function drop() {
+  playerOne.Inventory.slice(item);
+  newLocation.Item.push(item);
+}
+
+function use() {
+  //Placeholder
+}
+
+function lookRoom() {
+  console.log(newLocation.what);
+}
+
+function lookItem() {
+  console.log(item.what);
+}
+
+function checkStatus() {
+  console.log(playerOne.dispo);
+}
+
+function help() {}
+
+function menu() {}
+
+function quit() {
+  console.log(`Thanks for Playing ${playerOne.name}!`);
+  process.exit();
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-process.exit();
+   // console.log(word1);
